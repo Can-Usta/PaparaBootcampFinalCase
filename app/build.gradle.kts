fce9b224db719b112d85cf157ff8d4a177f03af8
Can Usta <canusta_45@hotmail.com>
2024-06-02 15:28:07 +0300

    Coil implemented.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,7 +68,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-    implementation(libs.kotlinx.serialization.json)
+
 
     // Lifecycle components
     implementation (libs.androidx.lifecycle.viewmodel.ktx)
